9f3899fca6a53a530c0740f48a8b09bc78739eaf
Ajay Nallani <nallaniajaychandra@gmail.com>
2024-03-21 00:14:21 -0400

    added waving animation

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,25 +1,25 @@
-import {Center, Text, ContactShadows, PresentationControls, Float, Environment, useGLTF, OrbitControls } from '@react-three/drei'
+import {Center, Text, ContactShadows, PresentationControls, Environment} from '@react-three/drei'
+import Ajay from './Ajay.jsx'
 
 export default function Experience()
 {
 
-    const ajay = useGLTF('https://models.readyplayer.me/61d7c56c1ee1d0492ab9cf5e.glb')
+   
     return <>
-
         <Environment preset='city' />
 
         <color args={ [ '#2f2f2f' ] } attach="background" />
         
         <PresentationControls 
-            rotation={ [ -0.25, -0.50, -0.15 ] }
+            global
+            rotation={ [ -0.25, -0.60, -0.15 ] }
             polar={ [ - 0.4, 0.2 ] }
             azimuth={ [ - 1, 0.75 ] }
             config={ { mass: 2, tension: 400 } }
             snap={ { mass: 4, tension: 400 } }
         >
             <Center bottom>
-                <primitive object={ ajay.scene }  
-                />
+                <Ajay />
             </Center>
             
         </PresentationControls>
